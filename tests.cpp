70d59cd9b58b4dd972e2ba127d62c0461ad23e77
ARNik <arnik@arnik.ru>
2026-03-08 20:36:20 +0300

    add tests for ip pool

--- tests.cpp
+++ tests.cpp
@@ -16,3 +16,28 @@ TEST(test_version, VersionSize)
 {
 	EXPECT_GT(std::string(GIT_VERSION).size(), 4);
 }
+
+#include "ip_pool.h"
+TEST(test_ip, CreateFromString)
+{
+	std::string ip_str = "192.168.0.1";
+	IP ip(ip_str);
+	EXPECT_EQ(ip.get_as_uint32(), 3232235521);
+	EXPECT_EQ(ip.get_as_string(), ip_str);
+}
+
+TEST(test_ip_pool, CreateFromString)
+{
+	IP_Pool p;
+	p.addIP("1.1.0.1	5	6");
+	p.addIP("10.0.0.0	110 1");
+	p.addIP("1.10.0.1	2	1");
+	p.addIP("1.1.10.1	22	0");
+
+	EXPECT_EQ(p.get_as_string(), "1.1.0.1\n10.0.0.0\n1.10.0.1\n1.1.10.1\n");
+	p.sort();
+	EXPECT_EQ(p.get_as_string(), "10.0.0.0\n1.10.0.1\n1.1.10.1\n1.1.0.1\n");
+	EXPECT_EQ(p.filter(10).get_as_string(), "10.0.0.0\n");
+	EXPECT_EQ(p.filter(1,1).get_as_string(), "1.1.10.1\n1.1.0.1\n");
+	EXPECT_EQ(p.filter_any(10).get_as_string(), "10.0.0.0\n1.10.0.1\n1.1.10.1\n");
+}
