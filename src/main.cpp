eabe72542733524adda71fcb247e95d3497caf3e
ARNik <arnik@arnik.ru>
2026-03-09 16:27:50 +0300

    add --version and --help keys

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,48 @@
 #include <iostream>
 #include <string>
 
+#include "../lib/cxxopts.hpp"
 #include "ip_pool.h"
+#include "version.h"
 
 // https://otusmetodist.yonote.ru/share/2cb07ace-75b1-48fe-b6fd-259b39f6025a/doc/2-filtraciya-ip-adresov-s8zT8mUKCH
 
 int main(int argc, char const* argv[])
 {
+	try {
+		// clang-format off
+		cxxopts::Options options("ip_filter", "Useful IP manipulation utility");
+		options.add_options()
+			("h,help", "Show this help information and exit")
+			("v,version", "Show version and exit")
+			;
+		// clang-format on
+
+		auto result = options.parse(argc, argv);
+
+		if (result.count("help")) {
+			std::cout << options.help() << std::endl;
+			return 0;
+		}
+
+		if (result.count("version")) {
+			std::cout << "ip_filter" << std::endl;
+			std::cout << "App version: " << GIT_VERSION << std::endl;
+			std::cout << "cxxopts version: "
+					  << static_cast<int>(cxxopts::version.major) << '.'
+					  << static_cast<int>(cxxopts::version.minor) << '.'
+					  << static_cast<int>(cxxopts::version.patch) << std::endl;
+
+			// TODO: добавить дату сборки
+			// std::cout << "Build date: " << BUILD_DATE << std::endl;
+			return 0;
+		}
+
+	} catch (const cxxopts::exceptions::exception& e) {
+		std::cerr << "Error parsing options: " << e.what() << std::endl;
+		return -1;
+	}
+
 	try {
 		IP_Pool p;
 
