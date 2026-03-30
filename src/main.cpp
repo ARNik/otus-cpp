01c9d36c57d61a95fc25107a1a5815432d163165
ARNik <arnik@arnik.ru>
2026-03-30 11:28:02 +0300

    wip

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
 #include <string>
 
+#include "../lib/allocator.hpp"
 #include "../lib/cxxopts.hpp"
 #include "version.h"
+// #include <memory>
 
 // https://otusmetodist.yonote.ru/share/2cb07ace-75b1-48fe-b6fd-259b39f6025a/doc/3-allocator-3hUxCcAj0a
+// https://en.cppreference.com/w/cpp/memory/allocator_traits.html
+
+
+unsigned long long fact(int n) {
+	return n <= 1 ? 1 : n * fact(n - 1);
+}
+
 
 int main(int argc, char const* argv[])
 {
@@ -42,7 +51,24 @@ int main(int argc, char const* argv[])
 		return -1;
 	}
 
+	//--------------------------------------------
+
 	try {
+
+		auto m1 = std::map<int, int>{};
+		for (int i = 0; i < 10; ++i) {
+			m1[i] = fact(i);
+		}
+
+		for (auto &el : m1) {
+			std::cout << el.first << " " << el.second << std::endl;
+		}
+
+		auto m = std::map<int, int, std::less<int>,
+						  MyAllocator<std::pair<const int, int>>>{};
+		for (int i = 0; i < 10; ++i) {
+			m[i] = 10;
+		}
 
 	} catch (const std::exception& e) {
 		std::cerr << e.what() << std::endl;
